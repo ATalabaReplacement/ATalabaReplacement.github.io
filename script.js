b2caaf12554fa6c0259b03153c64f393eb5ba9f5
Andrei Talaba <ATalabaReplacement@gmail.com>
2023-08-31 23:19:52 +0100

    Added css and js and tester

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    let elevation = $("#tabs").offset().top;
+    $(document).scroll(function () {
+        var $nav = $("#tabs");
+        $nav.toggleClass('scrolledTabs', $(this).scrollTop() > elevation);
+        $nav.toggleClass('normalTabs', $(this).scrollTop() <= elevation);
+    });
+
+    $(".tab").click(function() {
+        $(".focused").removeClass('focused');
+        $(".focusContent").removeClass('focusContent');
+        $(this).addClass('focused');
+        let name = $(this).attr('id').replace('Tab', '');
+        let content = "#" + name + "Content"
+        $(content).addClass('focusContent');
+        if ($(window).scrollTop() < 100) {
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $("#content").offset().top
+            }, 1000);
+        }
+    })
+})
